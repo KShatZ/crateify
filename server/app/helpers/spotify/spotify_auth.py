4b308981110bc0cae92dfbe6db257e6d3a61af93
KShatZ <kirillshats@gmail.com>
2024-11-15 15:24:15 -0500

    Refresh tokens functionality, auth headers rework

--- server/app/helpers/spotify/spotify_auth.py
+++ server/app/helpers/spotify/spotify_auth.py
@@ -7,9 +7,12 @@ from field_names import SPOTIFY, HTTP
 
 class SpotifyAuth():
 
-    def __init__(self, user_id):
-        self.user_id = user_id
-
+    @classmethod
+    def auth_headers(cls):
+        return {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Authorization": f"Basic {cls.base64_auth_string()}",
+        }
 
     @staticmethod
     def spotify_oauth_url():
@@ -59,10 +62,7 @@ class SpotifyAuth():
         """
 
         endpoint = SPOTIFY.TOKEN_ENDPOINT
-        headers = {
-            "Content-Type": "application/x-www-form-urlencoded",
-            "Authorization": f"Basic {SpotifyAuth.base64_auth_string()}",
-        }
+        headers = SpotifyAuth.auth_headers()
         params = {
             "grant_type": SPOTIFY.GRANT_TYPE_EXCHANGE_CODE,
             "code": code,
@@ -77,7 +77,7 @@ class SpotifyAuth():
             if request.status_code != HTTP.OK:
                 return None # Temporary
         
-            response_data = request.json()
+            response_data = request.json() # NOTE: This can throw, maybe seperate try for it?
 
             access_token = response_data.get("access_token")
             refresh_token = response_data.get("refresh_token")
@@ -89,12 +89,57 @@ class SpotifyAuth():
                 return None # Temporary
 
             tokens = {
-                "access_token": access_token,
-                "refresh_token": refresh_token,
+                "access": access_token,
+                "refresh": refresh_token,
                 "scope": scope,
             }
             return tokens
 
         # TODO - What to do on request fail
         except Exception as ExchangeError:
+            print(f"SpotifyAuth.request_auth_token() --- There was an error exchaning tokens... --- {ExchangeError}")
             return None #Temporary
+        
+
+    @staticmethod
+    def refresh_tokens(refresh_token):
+
+        if not refresh_token:
+            # TODO: Log
+            print("SpotifyAuth.refresh_tokens() --- Can't refresh... 'refresh_token' was not provided")
+            raise ValueError("Provide a value for refresh_token.") 
+
+        endpoint = SPOTIFY.TOKEN_ENDPOINT
+        headers = SpotifyAuth.auth_headers()
+        params = {
+            "grant_type": SPOTIFY.GRANT_TYPE_REFRESH_CODE,
+            "refresh_token": refresh_token
+        }
+
+        try:
+            request = requests.post(endpoint, headers=headers, params=params)
+            status = request.status_code
+
+            if status != HTTP.OK:
+                # TODO: What to do when refresh doesnt work -- retry a couple times??
+                print(f"SpotifyAuth.refresh_tokens() --- Failed to refresh access token.")
+                return None
+
+            response_data = request.json() # NOTE: This can throw
+
+            access_token = response_data.get("access_token")
+            refresh_token = response_data.get("refresh_token")
+
+            if not access_token:
+                # TODO: What to do if access_token not present -- Try again, if refresh is still gucci?
+                print(f"SpotifyAuth.refresh_tokens() --- Refresh request was succesful, but did not receive access token...")
+                return None
+            
+            tokens = {"access": access_token}
+            if refresh_token: # Sometimes a new refresh token is provided
+                tokens["refresh"] = refresh_token
+
+            return tokens
+        except Exception as RefreshError:
+            # TODO: What to do on error
+            print(f"SpotifyAuth.refresh_token() --- Error when refreshing tokens... --- {RefreshError}")
