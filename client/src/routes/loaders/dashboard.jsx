803ea0e0b47ecd169837a98f16c6267f5aa58ce4
KShatZ <kirillshats@gmail.com>
2024-11-22 14:12:30 -0500

    Refactor Dashboard route, in order to reflect the change in server auth and endpoint functionality

--- client/src/routes/loaders/dashboard.jsx
+++ client/src/routes/loaders/dashboard.jsx
@@ -1,20 +1,39 @@
+import { HTTP } from "../../field_names";
+
 
 // Need to get currentUser's playlists before rendering dashboard
 export default async function dashboardLoader() {
+    try {
+        const response = await fetch("/api/dashboard", {
+            method: "GET",
+            credentials: "include"
+        });
+        
+        const status = response.status;
+        switch (status) {
 
-    const response = await fetch("/api/user/playlists", {
-        method: "GET",
-        credentials: "include"
-    });
+            case HTTP.OK: {
+                const responseBody = await response.json();
+                return responseBody.data;
+            }
+                
+            case HTTP.SERVER_ERROR:
+                // TODO: What to do on server error
+                return {
+                    spotify_profile: {},
+                    playlists: {
+                        total: null,
+                        playlists: []
+                    }
+                }
 
-    // TODO - Need to implement other status codes server side first
-    if (response.status != 200) {
-        console.log("Error getting playlists...");
-        return [];
+            default:
+                // TODO
+                throw("Hit default case, got an unexpected status")
+        }
+
+    } catch (error) {
+        // TODO: Better error handling, look into react-router useRouteError()
+        throw "There was an error fetching dashboard data", error;
     }
-
-    const body = await response.json();
-
-    const playlists = body.data.playlists || [];
-    return playlists;
 }
