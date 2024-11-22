90428a9f7c610ec948317cc518c48e04afd624a9
KShatZ <kirillshats@gmail.com>
2024-11-22 14:11:37 -0500

    Change status codes to int

--- client/src/field_names.js
+++ client/src/field_names.js
@@ -5,14 +5,14 @@ export const CurrentUser = createContext();
 
 // ----- Server Requests ----- //
 export const HTTP = {
-    OK: "200",
-    CREATED: "201",
+    OK: 200,
+    CREATED: 201,
 
-    SEE_OTHER: "303",
+    SEE_OTHER: 303,
 
-    BAD_REQUEST: "400",
-    UNAUTHORIZED: "401",
-    CONFLICT: "409",
+    BAD_REQUEST: 400,
+    UNAUTHORIZED: 401,
+    CONFLICT: 409,
 
-    SERVER_ERROR: "500"
+    SERVER_ERROR: 500
 }
