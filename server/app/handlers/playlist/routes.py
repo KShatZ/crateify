97392c2f94162aa9f86ca85a1cf17b36a3eafc2b
KShatZ <kirillshats@gmail.com>
2024-12-07 13:35:49 -0500

    Barebones get playlist route complete, playlist class updates mainly for playlist serialization for client

--- server/app/handlers/playlist/routes.py
+++ server/app/handlers/playlist/routes.py
@@ -4,6 +4,7 @@ from flask_login import login_required, current_user
 from . import Playlist as bp
 from ...models.playlist import Playlist
 from ...helpers.response import create_response
+from field_names import HTTP
 
 
 @bp.get("/playlist/<playlist_id>")
@@ -11,21 +12,16 @@ from ...helpers.response import create_response
 def fetch_playlist(playlist_id):
 
     # NOTE: Perhaps some playlist_id check??
-
-    # If user hits this endpoint from dashboard, always guranteed to have most
-    # recent snap_id.
     snap_id = request.args.get("snap_id")
-
     playlist = Playlist(playlist_id, current_user, snap_id=snap_id)
-    playlist.load_playlist()
+    loaded = playlist.load_playlist()
 
+    if not loaded:
+        # TODO: Better Error Handling
+        return create_response(error=True, status_code=HTTP.SERVER_ERROR)
 
     # Populate response with playlist meta, tracks
-    # Send Response
-
-    return create_response()
-
-
+    return create_response(data=playlist.serialize())
 
     # In response, should signify if playlist was pulled from spotify and 
     # cached with most recent snap_id, so that the client can change the URL to the most recent
--- server/app/models/playlist.py
+++ server/app/models/playlist.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from typing import Optional
 
 from pymongo import MongoClient, UpdateOne
 
@@ -8,6 +9,8 @@ from .track import Track
 
 
 class Playlist:
+
+    SERIALIZE_IGNORE = ["_id", "href", "meta_updated_at", "owner", "primary_color", "uri", "type"]
 
     def __init__(self, playlist_id: str, user, snap_id: str = None):
 
@@ -51,18 +54,23 @@ class Playlist:
                     "id": 1, "artists": 1, "album": 1, "duration_ms": 1, "explicit": 1, "external_urls": 1, "external_ids": 1,
                 } # TODO: Class VAR and dict comprehension
                 track_docs = tracks_collection.find(filter, projection)
-                mongo.close()
-
+                
                 self.tracks.extend(Track(track) for track in track_docs)
+                mongo.close()
                 print(f"Playlist.load_playlist() -- User({self.user.id}) --- Playlist({self.id}) loaded from cache...")
+
                 return True
 
         mongo.close()
 
         # --- Load From Spotify --- # 
         playlist_meta, playlist_tracks = self._fetch_from_spotify()
-        updated = self._update_playlist(playlist_meta, playlist_tracks)
+        if not playlist_meta and not playlist_tracks:
+            # NOTE: Janky Way of handling invalid playlist id (and other errors)
+            print(f"Playlist.load_playlist() -- User({self.user.id}) --- Unable to load Playlist({self.id}) due to issue in pulling it's data from Spotify.")
+            return False
         
+        updated = self._update_playlist(playlist_meta, playlist_tracks)
         if not updated:
             print(f"Playlist.load_playlist() -- User({self.user.id}) --- There was a problem updating Playlist({self.id}) data during load")
             return False
@@ -71,6 +79,60 @@ class Playlist:
         return True
 
 
+    def get_duration_string(self) -> Optional[str]:
+        """Generated the total playlist duration in the format of hours minutes seconds.
+        This string is mainly generated to be used in the UI to show the total
+        playlist duration.
+
+        :return: String formated in hours minutes seconds for total duration of playlist
+        :rtype: Optional[str]
+        """
+
+        if self.meta is None:
+            return None
+        
+        duration_ms = self.meta.get("duration_ms")
+
+        h = duration_ms // 3600000
+        m = (duration_ms % 3600000) // 60000
+        s = (duration_ms % 60000) // 1000 # NOTE: Seconds are not rounded up like spotify does
+
+        minute_string = str(m) + " minutes " if m != 1 else str(m) + " minute "
+        second_string = str(s) + " seconds" if s != 1 else str(s) + " second"
+
+        if h == 0:
+            return minute_string + second_string
+        else:
+            hour_string = str(h) + " hours " if h != 1 else str(h) + " hour "
+            return hour_string + minute_string + second_string
+
+    
+    def serialize(self) -> Optional[dict]:
+        """Serializes the playlist data into a dict to be sent to 
+        the client.
+
+        :return: A dict containing meta and track information to be sent
+        to the client
+        :rtype: Optional[dict]
+        """
+
+        if not self.meta and not self.tracks:
+            return None
+
+        meta = {key:value for key, value in self.meta.items() if key not in Playlist.SERIALIZE_IGNORE}
+        meta["owner"] = {
+            "display_name": self.meta["owner"].get("display_name"),
+            "profile_url": self.meta["owner"]["external_urls"].get("spotify"),
+        }
+        meta["duration_string"] = self.get_duration_string()
+
+        tracks = [track.serialize() for track in self.tracks]
+
+        return {
+            "meta": meta,
+            "tracks": tracks
+        }
+    
     def _fetch_from_spotify(self) -> tuple:
         """Fetches fresh playlist data from Spotify API. Data includes playlist
         meta data along with all track objects belonging to the playlist. These
@@ -83,6 +145,9 @@ class Playlist:
 
         spotify_api = SpotifyAPI(self.user)
         playlist = spotify_api.get_playlist(self.id)
+
+        if not playlist:
+            return (None, None)
 
         playlist_tracks = playlist["tracks"].get("items", [])
 
@@ -115,19 +180,22 @@ class Playlist:
                 meta["spotify_id"] = value
 
             elif key == "images": # TODO: Update function for cover art -- Expires after 1 day
-
                 if value is None or len(value) == 0:
                     meta["cover_art"] = None
                 else:
                     cover_art = value[0].get("url")
                     meta["cover_art"] = cover_art
-
+            elif key == "external_urls":
+                meta["spotify_url"] = value.get("spotify")
+            elif key == "followers":
+                meta["followers"] = value.get("total")
             else:
                 meta[key] = value
 
         # --- Track Population --- # function?
         tracks_meta = []
         track_docs = []
+        total_duration_ms = 0
         for track in playlist_tracks:
             
             tracks_meta.append({
@@ -141,10 +209,13 @@ class Playlist:
             track_doc["playlists"] = [self.id]
             track_docs.append(track_doc)
 
+            total_duration_ms += track["track"].get("duration_ms")
+
         if meta["total_tracks"] != len(tracks_meta):
             print(f"Playlist._update_playlist() -- User({self.user.id}) Playlist({self.id}) --- Only processed {len(tracks_meta)} tracks instead of {meta['total_tracks']}")
 
         meta["tracks"] = tracks_meta
+        meta["duration_ms"] = total_duration_ms
         meta["meta_updated_at"] = datetime.now()
 
     
